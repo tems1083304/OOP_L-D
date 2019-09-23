1f0ad8b329140d74767c118906612b481fee05ad
tems1083304 <54797560+tems1083304@users.noreply.github.com>
2019-09-23 18:36:52 +0200

    Update restituzioni.java

--- univpm/src/main/java/esame/OOP/service/service/restituzioni.java
+++ univpm/src/main/java/esame/OOP/service/service/restituzioni.java
@@ -1,4 +1,4 @@
-package esame.OOP.service;
+package it.univpm.Progetto.service;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -7,16 +7,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import esame.OOP.service.statistiche;
-import model.Tabella;
+import it.univpm.Progetto.model.Tabella;
 
+
+
+ @Service
 public class restituzioni  {
+	 
 	
-	private List<Map> metadata = new ArrayList<>();
-	private List<Tabella> Ta = new ArrayList<>();
+	//private download Dow;
+	private List<Map> metadata;
+	private List<Tabella> Ta;
+	
+	@Autowired 
+	public restituzioni ( download Dow) {
+		this.Ta = Dow.PrendiTa();
+		this.metadata = Dow.PrendiMeta();
+	}
 	
 	
 	//METODO CHE MI RESTITUISCE LA LISTA DEGLI OGGETTI
